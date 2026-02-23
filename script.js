7a933fa872a10e0df702c5da7fff38b6f63f11c4
vladyslav321 <64689906+vladyslav321@users.noreply.github.com>
2026-02-23 16:01:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function highlightErrors(text, matches) {
+    let offset = 0;
+    let html = text;
+
+    matches.forEach(match => {
+        const start = match.offset + offset;
+        const end = start + match.length;
+
+        const wrong = html.substring(start, end);
+        const suggestion = match.replacements[0]?.value || "";
+
+        const replacementHTML =
+            `<span class="error" data-suggestion="${suggestion}">
+                ${wrong}
+            </span>`;
+
+        html =
+            html.substring(0, start) +
+            replacementHTML +
+            html.substring(end);
+
+        offset += replacementHTML.length - match.length;
+    });
+
+    document.getElementById("output").innerHTML = html;
+}
